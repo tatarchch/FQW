ecd317fe9c9c6b26d1ad26c4a2c19f8245e701f6
tatarchch <tatarnirov15277@mail.ru>
2024-09-09 19:44:51 +0300

    Add pom and DB properties

---
+++ src/main/java/com/example/FQW/FqwApplication.java
@@ -0,0 +1,13 @@
+package com.example.FQW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FqwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FqwApplication.class, args);
+	}
+
+}
