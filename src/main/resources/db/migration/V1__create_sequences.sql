746ce3bec1dc45840009e050f60e04161bf349cc
tatarchch <tatarnirov15277@mail.ru>
2024-11-12 17:36:52 +0300

    fix DB migrations

--- src/main/resources/db/migration/V1__create_sequences.sql
+++ src/main/resources/db/migration/V1__create_sequences.sql
@@ -1,7 +1,6 @@
 create schema if not exists prod;
 
-create sequence prod.calendar_seq
-INCREMENT by 1
+create sequence prod.calendar_seqINCREMENT by 1
 START with 1
 MINVALUE 1
 MAXVALUE 1000000
@@ -21,8 +20,7 @@ MINVALUE 1
 MAXVALUE 1000000
 CACHE 1;
 
-create sequence prod.place_seq
-INCREMENT by 1
+create sequence prod.place_seqINCREMENT by 1
 START with 1
 MINVALUE 1
 MAXVALUE 1000000
@@ -35,8 +33,7 @@ MINVALUE 1
 MAXVALUE 1000000
 CACHE 1;
 
-create sequence prod.service_seq
-INCREMENT by 1
+create sequence prod.service_seqINCREMENT by 1
 START with 1
 MINVALUE 1
 MAXVALUE 1000000
