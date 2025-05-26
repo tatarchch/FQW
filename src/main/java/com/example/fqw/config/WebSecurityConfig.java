d8d100e7b55bfbc533677177c6d8c20da009ee9a
tatarchch <tatarnirov15277@mail.ru>
2025-05-26 17:05:42 +0300

    ref securityConfig for cors v2

--- src/main/java/com/example/fqw/config/WebSecurityConfig.java
+++ src/main/java/com/example/fqw/config/WebSecurityConfig.java
@@ -78,10 +78,10 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        //config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedOrigins(List.of("*"));
-        //config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-        config.setAllowedMethods(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        //config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        //config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
